639b311bf1df7d77388158926a3159ce051129cc
svj0hn <sv.john@hotmail.com>
2017-02-22 16:02:25 +0100

    Added build script and changed folder structure

--- src/main/java/piglatin/PigLatinConverter.java
+++ src/main/java/piglatin/PigLatinConverter.java
@@ -1,4 +1,4 @@
-package converter;
+package piglatin;
 
 import java.util.Scanner;
 
---
+++ src/test/java/tests/PigLatinConverterTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import piglatin.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PigLatinConverterTest {
+  @Test
+  public void testConvertWordToPigLatin() {
+	String output = PigLatinConverter.convertWordToPigLatin("hello");
+    assertEquals(output, "Ellohay");
+  }
+}
