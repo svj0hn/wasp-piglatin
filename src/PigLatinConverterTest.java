6c169840ad47c4fe5e76cd58425669ef16cf4395
antekillen <andreas.bergstrom@liu.se>
2017-02-22 15:16:26 +0100

    Teststart

---
+++ src/PigLatinConverterTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import converter.*;
+
+public class PigLatinConverterTest {
+  @Test
+  public void testConvertWordToPigLatin() {
+	//String output = PigLatinConverter.convertWordToPigLatin("hello");
+    //assertEquals(output, "Ellohay");
+	assertEquals(1,1);
+  }
+}
