8c70f0a0e0b87bb646960024a56a050a86222ca3
svj0hn <sv.john@hotmail.com>
2017-02-22 13:52:52 +0100

    Added simple working code

---
+++ src/PigLatinConverter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PigLatinConverter {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String word = "";
+		
+		word = scan.next();
+		
+		System.out.println("Ellohay!");
+	}
+}
