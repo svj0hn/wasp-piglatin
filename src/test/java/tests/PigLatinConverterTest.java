4bf7bd3aa48e03dff59f03ee6f7d7667c1a1cb0c
antekillen <andreas.bergstrom@liu.se>
2017-02-24 21:28:26 +0100

    Fixed issue #19 + some cleanup
    Adressed issue #19 as well as cleaning up the testcases and code a
    bit....

--- src/test/java/tests/PigLatinConverterTest.java
+++ src/test/java/tests/PigLatinConverterTest.java
@@ -8,28 +8,34 @@ import org.junit.Test;
 
 public class PigLatinConverterTest {
 	@Test
-	public void testConvertWordConsToPigLatin() {
+	public void testInitialSingleConsonant() {
 		String output = PigLatinConverter.convertWordToPigLatin("hello");
 		assertEquals(output, "ellohay");
 	}
+	
+	@Test
+	public void testInitialMultipleConsonants() {
+		String output = PigLatinConverter.convertWordToPigLatin("cheers");
+		assertEquals(output, "eerschay");
+	}
 
 	@Test
-	public void testConvertWordVowelToPigLatin() {
+	public void testInitialSingleVovel() {
 		String output = PigLatinConverter.convertWordToPigLatin("aberration");
-		assertEquals(output, "aberrationay");
+		assertEquals(output, "aberrationway");
 	}
 	
 	@Test
 	public void testIsVowel() {
-		assertTrue(PigLatinConverter.isVowel('a') &&
-						PigLatinConverter.isVowel('e') &&
-						PigLatinConverter.isVowel('i') &&
-						PigLatinConverter.isVowel('o') &&
-						PigLatinConverter.isVowel('u') &&
-						PigLatinConverter.isVowel('y') &&
-						!(PigLatinConverter.isVowel('b') ||
-						PigLatinConverter.isVowel('k') ||
-						PigLatinConverter.isVowel('!') ||
-						PigLatinConverter.isVowel('#')));
+		assertTrue(PigLatinConverter.isVowel('a'));
+		assertTrue(PigLatinConverter.isVowel('e'));
+		assertTrue(PigLatinConverter.isVowel('i'));
+		assertTrue(PigLatinConverter.isVowel('o'));
+		assertTrue(PigLatinConverter.isVowel('u'));
+		assertTrue(PigLatinConverter.isVowel('y'));
+		assertFalse(PigLatinConverter.isVowel('b'));
+		assertFalse(PigLatinConverter.isVowel('k'));
+		assertFalse(PigLatinConverter.isVowel('!'));
+		assertFalse(PigLatinConverter.isVowel('#'));
 	}
 }
